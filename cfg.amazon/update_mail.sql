1716feb0deeee985a530d12e5f45d38d590aafeb
James Wang <jwang@vancasoft.com>
2018-01-02 21:54:30 -0800

    Added cfg.amazon, gerrit, gentoo and www

---
+++ cfg.amazon/update_mail.sql
@@ -0,0 +1,6 @@
+use mail;
+
+INSERT INTO `virtual_users` VALUES (10,4,'gbrjXHPZdFTjA','jerry@7deals.ca'),(11,4,'gpYvxI1N8UuUg','jason@7deals.ca'),(12,4,'ZttGUo./YB3Ao','steve@7deals.ca'),(13,4,'bz7393b9uXEZU','mary@7deals.ca'),(14,4,'obZyotQ1t4Uu2','dana@7deals.ca');
+
+
+INSERT INTO `virtual_aliases` VALUES (6,4,'info@7deals.ca','jerry@7deals.ca'),(7,4,'PayMe@7deals.ca','jerry@7deals.ca;jwang@7deals.ca'),(8,4,'admin@7deals.ca','jwang@7deals.ca;jerry@7deals.ca'),(9,4,'HireMe@7deals.ca','jerry@7deals.ca'),(10,4,'ListMe@7deals.ca','jerry@7deals.ca'),(11,4,'contact@7deals.ca','jerry@7deals.ca;jwang@7deals.ca'),(12,4,'Donate@7deals.ca','jerry@7deals.ca'),(13,4,'Today@7deals.ca','jerry@7deals.ca'),(14,4,'webmaster@7deals.ca','jwang@7deals.ca'),(15,2,'webmaster@localguider.com','jwang@localguider.com'),(16,4,'james@7deals.ca','jwang@7deals.ca');
