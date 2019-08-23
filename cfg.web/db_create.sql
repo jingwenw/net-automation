5980dc687c54d87253b8d7a849c66e2a61463958
James Wang <jwang@vancasoft.com>
2019-08-22 17:03:04 -0700

    Updated

--- cfg.web/db_create.sql
+++ cfg.web/db_create.sql
@@ -1,3 +1,45 @@
 drop database DB_NAME_;
 create database DB_NAME_;
+use  DB_NAME_;
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` FUNCTION `isCategoryOf`(`cpid` INT, `ppidTo` INT) RETURNS tinyint(1)
+    COMMENT 'To check if a given category pid is belongs to another'
+    BEGIN
+	DECLARE isa BOOLEAN;
+
+	DECLARE parent BOOLEAN;
+	DECLARE grand BOOLEAN;
+	DECLARE grandGrand BOOLEAN;
+	SET max_sp_recursion_depth=10;
+	IF cpid = ppidTo THEN
+	   	SET isa = true;
+	ELSEIF cpid = 0 THEN
+	        SET isa = false;
+	ELSE
+		SELECT ppid INTO parent FROM category WHERE pid = cpid;
+		IF parent = ppidTo THEN
+		   SET isa = true;
+		ELSEIF parent = 0 THEN
+		   SET isa = false;
+		ELSE
+		   SELECT ppid INTO grand FROM category WHERE pid = parent;
+		   IF grand = ppidTo THEN
+		      SET isa = true;
+		   ELSEIF grand = 0 THEN
+		      SET isa = false;
+		   ELSE
+    		      SELECT ppid INTO grandGrand FROM category	WHERE pid = grand;
+		      IF grandGrand = ppidTo THEN
+		      	 SET isa = true;
+		      ELSE
+		  	 SET isa = false;
+		      END IF;
+		   END IF;
+	       END IF;
+	END IF;
+    RETURN (isa);
+    END$$
+    DELIMITER ;
+
 grant all on DB_NAME_.* to 'jjwwang'@'localhost';
+
